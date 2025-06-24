817627a12bca33d2d8bb6249597e6f801fe66722
Monica-Vajrapu <monicavajrapu@gmail.com>
2025-06-24 07:18:46 +0530

    elevate commit

---
+++ New folder (2)/backend/server.mjs
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath  } from "url";
+
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+// Serve fronten
+app.use(express.static(path.join(__dirname, "public")));
+app.use(cors());
+app.use(express.json());
+
+// Test summary generator (for now)
+app.post("/api/ai-summary", (req, res) => {
+  const { role, experience, skills } = req.body;
+  console.log("✅ Received:", req.body);
+  res.json({
+    summary: `Experienced ${role} with ${experience} years in ${skills.join(', ')}.`
+  });
+});
+
+app.listen(3000, () => {
+  console.log("✅ Server running at http://localhost:3000");
+});
